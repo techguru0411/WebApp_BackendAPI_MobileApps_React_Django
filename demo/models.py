1cdd9e1bbfe1e0d431a0c5f50ebd396e88d19f45
techguru0411 <techguru0411@gmail.com>
2020-03-08 17:14:45 +0530

    implemented one to one

--- demo/models.py
+++ demo/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here..
+
+class BookNumber(models.Model):
+    isbn_10 = models.CharField(max_length=10, blank=True)
+    isbn_13 = models.CharField(max_length=13, blank=True)
+
 class Book(models.Model):
 
     title = models.CharField(max_length=36, blank=False, unique=True)
@@ -12,6 +17,8 @@ class Book(models.Model):
     is_published = models.BooleanField(default=False)
 
     cover = models.ImageField(upload_to='covers/', blank=True)
+
+    number = models.OneToOneField(BookNumber, null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- demo/serializers.py
+++ demo/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from demo.models import Book
+from demo.models import Book, BookNumber
 
+class BookNumberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookNumber
+        fields = ['id', 'isbn_10', 'isbn_13']
 
 class BookSerializer(serializers.ModelSerializer):
+    number = BookNumberSerializer(many=False)
+
     class Meta:
         model = Book
-        fields = ['id', 'title', 'description', 'price', 'published', 'is_published']
+        fields = ['id', 'title', 'description', 'price', 'published', 'is_published', 'number']
