db7f70f2ea363bf1a8016492d58e53e52c61acac
techguru0411 <techguru0411@gmail.com>
2020-04-01 12:10:43 +0530

    completed seciton 4

---
+++ Section 4 React Introduction/src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <div>
+        <h1>{props.info}</h1>
+        <h1>Hi, there</h1>
+        </div>
+
+    )
+}
+
+export default Header;
